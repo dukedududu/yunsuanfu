4fb885033135fea436bcda3016fcd5e4c6640977
依凡 张 <2238406324@qq.com>
2019-11-17 16:14:10 +0800

    添加项目文件。

---
+++ jjj.py
@@ -0,0 +1,15 @@
+
+a=11;b=20
+c=0
+c=a+b
+print('c的值为',c)
+c=a-b
+print('c的值为',c)
+c=a//b #//是整除  ，/是除出来可以是小数
+print('c的值为',c)
+c=a**b
+print('c的值为',c)
+c=a/b
+print('c的值为',c)
+c=a*b
+print('c的值为',c)
